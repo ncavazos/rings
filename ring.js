60c3c9c78c71bbcf147d3558d6c26056431fbf1c
cata <ncavazos18@gmail.com>
2023-01-31 12:46:14 -0600

    Update ring.js

--- ring.js
+++ ring.js
@@ -46,3 +46,47 @@ setFunction({
 
     return vec4(color,1.0);`
 })
+setFunction({
+  name: 'm_ring',
+  type: 'src',
+  inputs: [
+    {
+      type: 'float',
+      name: 'thresh1',
+      default: .3,
+    },
+    {
+      type: 'float',
+      name: 'thresh2',
+      default: .36,
+    },
+    {
+      type: 'float',
+      name: 'thresh3',
+      default: .28,
+    },
+    {
+      type: 'float',
+      name: 'thresh4',
+      default: .31,
+    },
+  ],
+
+    float pct = 0.0;
+    pct = distance(st,vec2(0.5));
+    
+    //smooth original circle
+    float x = 0.0;
+    x = smoothstep(.3,.36,pct);
+    
+    //smooth second circle
+    float y = 0.0;
+    y = smoothstep(.28,.31,pct);
+    
+    //gradient
+    float sync = u_time/1.0;
+    vec3 gr = vec3(abs(sin(st.x*3.14+u_time)));
+    
+    vec3 color = vec3(1.0-x-(1.0-y)-(gr));
+    return vec4(color,1.0);`
+})
